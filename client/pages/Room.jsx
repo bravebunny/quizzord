80428486da5d3a5f8c53564d1213104f927afee3
Francisco Baio Dias <xicombd@gmail.com>
2015-07-02 20:24:52 +0100

    Added room

--- client/pages/Room.jsx
+++ client/pages/Room.jsx
@@ -1,8 +1,13 @@
 var React = require('react')
 var app = require('ampersand-app')
 
+var EnrollForm = require('../views/room/EnrollForm.jsx')
+
 module.exports = React.createClass({
   displayName: 'Room',
+  getInitialState: function () {
+    return { state: 'anonymous' }
+  },
   componentDidMount: function () {
     var self = this
     app.socket.on('play_track', function (data) {
@@ -17,10 +22,23 @@ module.exports = React.createClass({
       })
     })
   },
+  handleEnroll: function (data) {
+    console.log('enroll', data)
+  },
   render: function () {
+    var content
+
+    if (this.state.state === 'anonymous') {
+      content = (
+        <EnrollForm onChange={this.handleEnroll}/>
+      )
+    }
+
     return (
       <div>
         <h1>Room {this.props.room}</h1>
+
+        {content}
       </div>
     )
   }
---
+++ client/views/room/EnrollForm.jsx
@@ -0,0 +1,22 @@
+var React = require('react')
+
+module.exports = React.createClass({
+  displayName: 'EnrollForm',
+  handleSubmit: function (ev) {
+    ev.preventDefault()
+    var name = this.refs.name.getDOMNode().value
+    this.props.onChange({ name: name })
+  },
+  render: function () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Enroll</h2>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input id='name' type='name' ref='name' className='form-control' placeholder='Name'/>
+        </div>
+        <button type='submit' className='btn btn-default'>Submit</button>
+      </form>
+    )
+  }
+})
